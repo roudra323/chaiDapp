afc2674728b74a29622d68b6e84bad253e918204
roudra323 <asirroudra@gmail.com>
2023-04-01 02:27:59 +0600

    added contract

---
+++ contracts/chai.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract chai {
+    struct Memo {
+        string name;
+        string message;
+        uint timestamp;
+        address from;
+    }
+
+    Memo[] memos;
+    address payable owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function buyChai(string memory _name, string memory _message)
+        public
+        payable
+    {
+        require(
+            msg.value > 0,
+            "You can't pay 0 . Try again with the value greater than 0"
+        );
+        owner.transfer(msg.value);
+        memos.push(Memo(_name, _message, block.timestamp, msg.sender));
+    }
+
+    function getMemos() public view returns (Memo[] memory) {
+        return memos;
+    }
+}
